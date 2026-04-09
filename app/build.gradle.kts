2a8c93b9adb4edf88b9346b14adab56731ad5eab
marcomorosi.dev <marcomorosi.dev@gmail.com>
2026-04-09 19:21:22 +0200

    updated version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cuscus.cussiparking"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
